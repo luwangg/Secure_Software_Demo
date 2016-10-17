bb7f2bc4d723f841025f4ad60778e8baff09f3e8
Maximilian Pachl <m@ximilian.info>
2016-10-18 01:53:46 +0200

    added heroku configuration files

--- software/src/main/java/edu/hm/bugcoin/web/MvcConfiguration.java
+++ software/src/main/java/edu/hm/bugcoin/web/MvcConfiguration.java
@@ -9,6 +9,7 @@ package edu.hm.bugcoin.web;
 import edu.hm.bugcoin.web.auth.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
@@ -22,5 +23,11 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter
     public void addInterceptors(final InterceptorRegistry registry)
     {
         registry.addInterceptor(new AuthInterceptor());
+    }
+
+    @Override public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/public/").setCachePeriod(31556926);
     }
 }
