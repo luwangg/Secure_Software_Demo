826a9ad55984bdf1bccdabb97d90644a0bb9173f
Maximilian Pachl <m@ximilian.info>
2016-10-18 20:49:36 +0200

    fixed merge

--- software/src/main/resources/import.sql
+++ software/src/main/resources/import.sql
@@ -4,16 +4,16 @@
 -- bart simpson
 -- password plaintext: hans
 INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('bart90', 'Simpson', 'Bart', 'ABC-Street', '86461', 'Springfield', 'bart@simpson.com', '$2a$10$F35NUXlsKrhIcZIZqR3myu0m9suN3PXp1GXKMZFMKAbStXFFJYT7G', '$2a$10$F35NUXlsKrhIcZIZqR3myu', '26MOCFUXZ5G6MJ2K')
-INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (1, 980234, 7518)
-INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (1, 516842, 47.50)
+INSERT INTO bankaccount(customer_id, account_number) VALUES (1, 980234)
+INSERT INTO bankaccount(customer_id, account_number) VALUES (1, 516842)
 
 -- lisa simpson
 -- password plaintext: hans
 INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('lisa', 'Simpson', 'Lisa', 'Mapledrive 24', '86461', 'Springfield', 'lisa@simpson.com', '$2a$10$F35NUXlsKrhIcZIZqR3myu0m9suN3PXp1GXKMZFMKAbStXFFJYT7G', '$2a$10$F35NUXlsKrhIcZIZqR3myu', '26MOCFUXZ5G6MJ2K')
-INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (2, 849544, 11.25)
+INSERT INTO bankaccount(customer_id, account_number) VALUES (2, 849544)
 
 -- voucher accounter
-INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('voucher', 'Account', 'Voucher', '---', '---', '---', '---', '', '', '')
+INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('voucher', 'Voucher', 'Bugcoin', '---', '---', '---', '---', '', '', '')
 INSERT INTO bankaccount(customer_id, account_number) VALUES (3, 100000)
 
 -- initial account balance (voucher)
