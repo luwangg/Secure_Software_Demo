f18d3f01f4887ed75004fae12ac95e11fdfa4694
Marvin <shreaker.figa@gmx.net>
2016-10-18 17:41:11 +0200

    update import.sql

--- software/src/main/resources/import.sql
+++ software/src/main/resources/import.sql
@@ -2,12 +2,14 @@
 -- Sample dataset
 
 -- bart simpson
-INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('bart90', 'Simpson', 'Bart', 'ABC-Street', '86461', 'Springfield', 'bart@simpson.com', '123', '26MOCFUXfafaZ5G6MJ2K', '26MOCFUXZ5G6MJ2K')
+-- password plaintext: hans
+INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('bart90', 'Simpson', 'Bart', 'ABC-Street', '86461', 'Springfield', 'bart@simpson.com', '$2a$10$F35NUXlsKrhIcZIZqR3myu0m9suN3PXp1GXKMZFMKAbStXFFJYT7G', '$2a$10$F35NUXlsKrhIcZIZqR3myu', '26MOCFUXZ5G6MJ2K')
 INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (1, 980234, 7518)
 INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (1, 516842, 47.50)
 
 -- lisa simpson
-INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('lisa', 'Simpson', 'Lisa', 'Mapledrive 24', '86461', 'Springfield', 'lisa@simpson.com', '123', '26MOCFUXZfafafa5G6MJ2K', '26MOCFUXZ5G6MJ2K')
+-- password plaintext: hans
+INSERT INTO customer(nickname, lastname, firstname, street, postCode, city, email, password, salt, otpkey) VALUES ('lisa', 'Simpson', 'Lisa', 'Mapledrive 24', '86461', 'Springfield', 'lisa@simpson.com', '$2a$10$F35NUXlsKrhIcZIZqR3myu0m9suN3PXp1GXKMZFMKAbStXFFJYT7G', '$2a$10$F35NUXlsKrhIcZIZqR3myu', '26MOCFUXZ5G6MJ2K')
 INSERT INTO bankaccount(customer_id, accountnumber, balance) VALUES (2, 849544, 11.25)
 
 
